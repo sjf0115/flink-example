8762307f3d83dcf744aa8e57bd631093140f5182
sjf0115 <1203745031@qq.com>
2025-06-13 23:32:00 +0800

    MOD: FileSystemCheckpointStorageExample

--- flink-example-1.13/src/main/java/com/flink/example/stream/state/checkpoint/FileSystemCheckpointStorageExample.java
+++ flink-example-1.13/src/main/java/com/flink/example/stream/state/checkpoint/FileSystemCheckpointStorageExample.java
@@ -2,11 +2,9 @@ package com.flink.example.stream.state.checkpoint;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.runtime.state.storage.JobManagerCheckpointStorage;
+import org.apache.flink.runtime.state.storage.FileSystemCheckpointStorage;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -14,19 +12,16 @@ import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
 /**
- * 功能：JobManagerCheckpointStorage 示例
+ * 功能：FileSystemCheckpointStorage 示例
+ *      通过 Savepoint 停止作业恢复状态
  * 作者：SmartSi
  * CSDN博客：https://smartsi.blog.csdn.net/
  * 公众号：大数据生态
- * 日期：2025/6/11 23:23
+ * 日期：2025/6/13 20:24
  */
-public class JobManagerCheckpointStorageExample {
-    private static final Logger LOG = LoggerFactory.getLogger(RestoreCheckpointExample.class);
-    private static final Long errorTimeMillis = 1749567120000L;
+public class FileSystemCheckpointStorageExample {
+    private static final Logger LOG = LoggerFactory.getLogger(FileSystemCheckpointStorageExample.class);
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -35,14 +30,9 @@ public class JobManagerCheckpointStorageExample {
         // 每20s一次Checkpoint
         env.enableCheckpointing(20 * 1000);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        // JobManagerCheckpointStorage
-        env.getCheckpointConfig().setCheckpointStorage(new JobManagerCheckpointStorage());
-
-        // 重启策略
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(
-                3, // 重启最大次数
-                Time.of(10, TimeUnit.SECONDS) // 重启时间间隔
-        ));
+        // FileSystemCheckpointStorage
+        String checkpointPath = "hdfs://localhost:9000/flink/checkpoint";
+        env.getCheckpointConfig().setCheckpointStorage(new FileSystemCheckpointStorage(checkpointPath));
 
         // Socket 输入
         DataStream<String> stream = env.socketTextStream("localhost", 9100, "\n");
@@ -53,16 +43,7 @@ public class JobManagerCheckpointStorageExample {
             public void flatMap(String input, Collector<Tuple2<String, Long>> out) throws Exception {
                 for (String word : input.split("\\s")) {
                     LOG.info("word: {}", word);
-                    long currentTimeMillis = System.currentTimeMillis();
-                    // 整分输入的 ERROR 抛出异常模拟脏数据导致作业Failover
-                    if (Objects.equals(word, "ERROR")) {
-                        if (currentTimeMillis <= errorTimeMillis) {
-                            throw new RuntimeException("模拟脏数据导致作业Failover");
-                        }
-                        // 非整分输入的 ERROR 自动忽略
-                    } else {
-                        out.collect(Tuple2.of(word, 1L));
-                    }
+                    out.collect(Tuple2.of(word, 1L));
                 }
             }
         }).keyBy(new KeySelector<Tuple2<String, Long>, String>() {
@@ -79,6 +60,6 @@ public class JobManagerCheckpointStorageExample {
 
         wordStream.print();
 
-        env.execute("JobManagerCheckpointStorageExample");
+        env.execute("FileSystemCheckpointStorageExample");
     }
 }
