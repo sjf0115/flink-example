d7c5c7b4c1f721ba8498e7478f072d97d0834f3e
sjf0115 <1203745031@qq.com>
2025-08-23 23:57:35 +0800

    MOD: CustomPeriodicWatermarkGeneratorExample

--- flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkGeneratorExample.java
+++ flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkGeneratorExample.java
@@ -3,15 +3,20 @@ package com.flink.example.stream.watermark;
 import com.flink.common.bean.WordCountTimestamp;
 import com.flink.example.stream.source.custom.WordCountOutOfOrderSource;
 import org.apache.flink.api.common.eventtime.*;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 /**
  * 功能：自定义实现周期性 WatermarkStrategy 通过
@@ -32,116 +37,69 @@ public class CustomPeriodicWatermarkGeneratorExample {
         // 定义 Watermark 策略 - 自定义周期性 Watermark
         DataStream<WordCountTimestamp> words = source
                 // 使用自定义 WatermarkStrategy
-                .assignTimestampsAndWatermarks(WatermarkStrategy.forGenerator(new CustomWatermarkGenerator()));
-
-
-
-        // 提取时间戳、生成Watermark
-        DataStream<Tuple3<String, Long, Integer>> watermarkStream = input.assignTimestampsAndWatermarks(
-                new WatermarkStrategy<Tuple3<String, Long, Integer>>() {
-                    @Override
-                    public WatermarkGenerator<Tuple3<String, Long, Integer>> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
-                        return new CustomWatermarkGenerator();
-                    }
-                }.withTimestampAssigner(new SerializableTimestampAssigner<Tuple3<String, Long, Integer>>() {
-                    @Override
-                    public long extractTimestamp(Tuple3<String, Long, Integer> element, long recordTimestamp) {
-                        return element.f1;
-                    }
-                })
-                // 必须指定TimestampAssigner否则报错
-        );
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.forGenerator(new WatermarkGeneratorSupplier<WordCountTimestamp>() {
+                            @Override
+                            public WatermarkGenerator<WordCountTimestamp> createWatermarkGenerator(Context context) {
+                                return new CustomWatermarkGenerator(Duration.ofSeconds(5));
+                            }
+                        }).withTimestampAssigner(new SerializableTimestampAssigner<WordCountTimestamp>() {
+                            @Override
+                            public long extractTimestamp(WordCountTimestamp wc, long l) {
+                                return wc.getTimestamp();
+                            }
+                        })
+                );
 
         // 分组求和
-        DataStream<Tuple2<String, Integer>> result = watermarkStream
-                // 格式转换
-                .map(tuple -> Tuple2.of(tuple.f0, tuple.f2)).returns(Types.TUPLE(Types.STRING, Types.INT))
+        DataStream<WordCountTimestamp> result = words
                 // 分组
-                .keyBy(tuple -> tuple.f0)
-                // 窗口大小为10分钟、滑动步长为5分钟的滑动窗口
-                .timeWindow(Time.minutes(10), Time.minutes(5))
-                // 分组求和
-                .sum(1);
+                .keyBy(new KeySelector<WordCountTimestamp, String>() {
+                    @Override
+                    public String getKey(WordCountTimestamp wc) throws Exception {
+                        return wc.getWord();
+                    }
+                })
+                // 窗口大小为1分钟的滚动窗口
+                .window(TumblingEventTimeWindows.of(Time.minutes(1)))
+                // 窗口计算：求和
+                .reduce(new ReduceFunction<WordCountTimestamp>() {
+                    @Override
+                    public WordCountTimestamp reduce(WordCountTimestamp wc1, WordCountTimestamp wc2) throws Exception {
+                        String ids = wc1.getId() + "," + wc2.getId();
+                        int frequency = wc1.getFrequency() + wc2.getFrequency();
+                        long timestamp = Math.max(wc1.getTimestamp(), wc2.getFrequency());
+                        return new WordCountTimestamp(ids, wc1.getWord(), frequency, timestamp);
+                    }
+                });
 
         result.print();
-        env.execute("PeriodicWatermarkGeneratorExample");
+        env.execute("CustomPeriodicWatermarkGeneratorExample");
     }
 
-    /**
-     * 自定义 Periodic WatermarkGenerator
-     */
-    private static class CustomWatermarkGenerator implements WatermarkGenerator<Tuple3<String, Long, Integer>> {
+    // 自定义 Periodic WatermarkGenerator
+    private static class CustomWatermarkGenerator implements WatermarkGenerator<WordCountTimestamp> {
+        // 最大时间戳
+        private long maxTimestamp;
+        // 最大乱序时间
+        private final long outOfOrderMillis;
 
-        private final long maxOutOfOrderness = 600000; // 10分钟
-        private long currentMaxTimestamp = Long.MIN_VALUE + maxOutOfOrderness + 1;
-        // 前一个Watermark时间戳
-        private long preWatermarkTimestamp = Long.MIN_VALUE;
-
-        @Override
-        public void onEvent(Tuple3<String, Long, Integer> event, long eventTimestamp, WatermarkOutput output) {
-            currentMaxTimestamp = Math.max(currentMaxTimestamp, eventTimestamp);
-            String currentTime = DateUtil.timeStamp2Date(eventTimestamp, "yyyy-MM-dd HH:mm:ss");
-            String currentMaxTime = DateUtil.timeStamp2Date(currentMaxTimestamp, "yyyy-MM-dd HH:mm:ss");
-
-            LOG.info("[INFO] Key: {}, CurrentTimestamp: [{}|{}], CurrentMaxTimestamp: [{}|{}]",
-                    event.f0, currentTime, eventTimestamp, currentMaxTime, currentMaxTimestamp
-            );
+        public CustomWatermarkGenerator(Duration maxOutOfOrderMillis) {
+            this.outOfOrderMillis = maxOutOfOrderMillis.toMillis();
+            // 起始最小 Watermark 为 Long.MIN_VALUE.
+            this.maxTimestamp = Long.MIN_VALUE + outOfOrderMillis + 1;
         }
 
+        // 最大时间戳
+        @Override
+        public void onEvent(WordCountTimestamp wc, long eventTimestamp, WatermarkOutput output) {
+            maxTimestamp = Math.max(maxTimestamp, wc.getTimestamp());
+        }
+
+        // 周期性生成 Watermark
         @Override
         public void onPeriodicEmit(WatermarkOutput output) {
-            Watermark watermark = new Watermark(currentMaxTimestamp - maxOutOfOrderness - 1);
-            Long watermarkTimestamp = watermark.getTimestamp();
-            // Watermark发生变化才输出Log
-            if(watermarkTimestamp > preWatermarkTimestamp) {
-                LOG.info("[INFO] Watermark: [{}|{}]", watermark.getFormattedTimestamp(), watermark.getTimestamp());
-            }
-            preWatermarkTimestamp = watermarkTimestamp;
-            // 输出Watermark
-            output.emitWatermark(watermark);
+            output.emitWatermark(new Watermark(maxTimestamp - outOfOrderMillis - 1));
         }
     }
-
-    /**
-     * TimeLagWatermarkGenerator
-     */
-    public class TimeLagWatermarkGenerator implements WatermarkGenerator<Tuple3<String, Long, Integer>> {
-
-        private final long maxTimeLag = 5000; // 5 seconds
-
-        @Override
-        public void onEvent(Tuple3<String, Long, Integer> event, long eventTimestamp, WatermarkOutput output) {
-            // don't need to do anything because we work on processing time
-        }
-
-        @Override
-        public void onPeriodicEmit(WatermarkOutput output) {
-            output.emitWatermark(new Watermark(System.currentTimeMillis() - maxTimeLag));
-        }
-    }
-    // 输入样例
-//    A,2021-02-27 12:07:01
-//    B,2021-02-27 12:08:01
-//    A,2021-02-27 12:14:01
-//    C,2021-02-27 12:09:01
-//    C,2021-02-27 12:15:01
-//    A,2021-02-27 12:08:01
-//    B,2021-02-27 12:13:01
-//    B,2021-02-27 12:21:01
-//    D,2021-02-27 12:04:01
-//    B,2021-02-27 12:26:01
-//    B,2021-02-27 12:17:01
-//    D,2021-02-27 12:09:01
-//    C,2021-02-27 12:30:01
-
-    // 结果
-//    (A,2)
-//    (B,1)
-//    (C,1)
-//    (A,3)
-//    (C,1)
-//    (B,2)
-//    (B,2)
-//    (C,1)
-//    (A,1)
 }
