3ab1021a59e82197ae045a4944d1c21dcc9c3cd6
sjf0115 <1203745031@qq.com>
2025-08-24 17:47:09 +0800

    MOD: CustomPeriodicWatermarkStrategyExample

--- flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkStrategyExample.java
+++ flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkStrategyExample.java
@@ -16,7 +16,8 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 
 /**
- * 功能：自定义实现周期性 WatermarkStrategy
+ * 功能：自定义实现周期性 Watermark
+ *      通过自定义 WatermarkStrategy 接口实现
  * 作者：SmartSi
  * CSDN博客：https://smartsi.blog.csdn.net/
  * 公众号：大数据生态
@@ -34,7 +35,20 @@ public class CustomPeriodicWatermarkStrategyExample {
         // 定义 Watermark 策略 - 自定义周期性 Watermark
         DataStream<WordCountTimestamp> words = source
                 // 使用自定义 WatermarkStrategy
-                .assignTimestampsAndWatermarks(new CustomWatermarkStrategy(Duration.ofSeconds(5)));
+                .assignTimestampsAndWatermarks(new WatermarkStrategy<WordCountTimestamp>() {
+                    // 创建 Watermark 生成器
+                    @Override
+                    public WatermarkGenerator<WordCountTimestamp> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
+                        // 自定义周期性 Watermark 生成器
+                        return new CustomPeriodicGenerator(Duration.ofSeconds(5));
+                    }
+                    // 创建时间戳分配器
+                    @Override
+                    public TimestampAssigner<WordCountTimestamp> createTimestampAssigner(TimestampAssignerSupplier.Context context) {
+                        // 自定义时间戳分配器
+                        return new CustomTimestampAssigner();
+                    }
+                });
 
         // 分组求和
         DataStream<WordCountTimestamp> result = words
@@ -60,27 +74,6 @@ public class CustomPeriodicWatermarkStrategyExample {
 
         result.print();
         env.execute("CustomPeriodicWatermarkStrategyExample");
-    }
-
-    // 自定义 WatermarkStrategy
-    public static class CustomWatermarkStrategy implements WatermarkStrategy<WordCountTimestamp> {
-        private final Duration maxOutOfOrderMillis;
-
-        public CustomWatermarkStrategy(Duration maxOutOfOrderMillis) {
-            this.maxOutOfOrderMillis = maxOutOfOrderMillis;
-        }
-
-        // 创建 Watermark 生成器
-        @Override
-        public WatermarkGenerator<WordCountTimestamp> createWatermarkGenerator(WatermarkGeneratorSupplier.Context context) {
-            return new CustomPeriodicGenerator(maxOutOfOrderMillis);
-        }
-
-        // 创建时间戳分配器
-        @Override
-        public TimestampAssigner<WordCountTimestamp> createTimestampAssigner(TimestampAssignerSupplier.Context context) {
-            return new CustomTimestampAssigner();
-        }
     }
 
     // 自定义周期性 Watermark 生成器
@@ -117,3 +110,20 @@ public class CustomPeriodicWatermarkStrategyExample {
         }
     }
 }
+// 输出结果
+//17:38:24,856 INFO  WordCountOutOfOrderSource [] - id: 1, word: a, frequency: 2, eventTime: 1662303772840|2022-09-04 23:02:52
+//17:38:25,863 INFO  WordCountOutOfOrderSource [] - id: 2, word: a, frequency: 1, eventTime: 1662303770844|2022-09-04 23:02:50
+//17:38:26,870 INFO  WordCountOutOfOrderSource [] - id: 3, word: a, frequency: 3, eventTime: 1662303773848|2022-09-04 23:02:53
+//17:38:27,874 INFO  WordCountOutOfOrderSource [] - id: 4, word: a, frequency: 2, eventTime: 1662303774866|2022-09-04 23:02:54
+//17:38:28,879 INFO  WordCountOutOfOrderSource [] - id: 5, word: a, frequency: 1, eventTime: 1662303777839|2022-09-04 23:02:57
+//17:38:29,884 INFO  WordCountOutOfOrderSource [] - id: 6, word: a, frequency: 2, eventTime: 1662303784887|2022-09-04 23:03:04
+//17:38:30,891 INFO  WordCountOutOfOrderSource [] - id: 7, word: a, frequency: 3, eventTime: 1662303776894|2022-09-04 23:02:56
+//17:38:31,894 INFO  WordCountOutOfOrderSource [] - id: 8, word: a, frequency: 1, eventTime: 1662303786891|2022-09-04 23:03:06
+//WordCountTimestamp{id='1,2,3,4,5,7', word='a', frequency=12, timestamp=1662303772840}
+//17:38:32,900 INFO  WordCountOutOfOrderSource [] - id: 9, word: a, frequency: 5, eventTime: 1662303778877|2022-09-04 23:02:58
+//17:38:33,907 INFO  WordCountOutOfOrderSource [] - id: 10, word: a, frequency: 4, eventTime: 1662303791904|2022-09-04 23:03:11
+//17:38:34,910 INFO  WordCountOutOfOrderSource [] - id: 11, word: a, frequency: 1, eventTime: 1662303795918|2022-09-04 23:03:15
+//17:38:35,912 INFO  WordCountOutOfOrderSource [] - id: 12, word: a, frequency: 6, eventTime: 1662303779883|2022-09-04 23:02:59
+//17:38:36,919 INFO  WordCountOutOfOrderSource [] - id: 13, word: a, frequency: 2, eventTime: 1662303846254|2022-09-04 23:04:06
+//WordCountTimestamp{id='6,8,10,11', word='a', frequency=8, timestamp=1662303784887}
+//WordCountTimestamp{id='13', word='a', frequency=2, timestamp=1662303846254}
