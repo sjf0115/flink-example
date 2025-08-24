082c0f0b6562ad5fd063335daa045627ebd0854c
sjf0115 <1203745031@qq.com>
2025-08-24 17:54:42 +0800

    MOD: CustomPeriodicWatermarkGeneratorExample

--- flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkGeneratorExample.java
+++ flink-example-1.13/src/main/java/com/flink/example/stream/watermark/CustomPeriodicWatermarkGeneratorExample.java
@@ -4,10 +4,7 @@ import com.flink.common.bean.WordCountTimestamp;
 import com.flink.example.stream.source.custom.WordCountOutOfOrderSource;
 import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -19,7 +16,8 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 
 /**
- * 功能：自定义实现周期性 WatermarkStrategy 通过
+ * 功能：自定义实现周期性 Watermark
+ *         通过 WatermarkStrategy.forGenerator 自定义实现 WatermarkGenerator
  * 作者：SmartSi
  * CSDN博客：https://smartsi.blog.csdn.net/
  * 公众号：大数据生态
@@ -103,3 +101,20 @@ public class CustomPeriodicWatermarkGeneratorExample {
         }
     }
 }
+// 输出结果
+//17:51:31,594 INFO  WordCountOutOfOrderSource [] - id: 1, word: a, frequency: 2, eventTime: 1662303772840|2022-09-04 23:02:52
+//17:51:32,598 INFO  WordCountOutOfOrderSource [] - id: 2, word: a, frequency: 1, eventTime: 1662303770844|2022-09-04 23:02:50
+//17:51:33,602 INFO  WordCountOutOfOrderSource [] - id: 3, word: a, frequency: 3, eventTime: 1662303773848|2022-09-04 23:02:53
+//17:51:34,608 INFO  WordCountOutOfOrderSource [] - id: 4, word: a, frequency: 2, eventTime: 1662303774866|2022-09-04 23:02:54
+//17:51:35,614 INFO  WordCountOutOfOrderSource [] - id: 5, word: a, frequency: 1, eventTime: 1662303777839|2022-09-04 23:02:57
+//17:51:36,621 INFO  WordCountOutOfOrderSource [] - id: 6, word: a, frequency: 2, eventTime: 1662303784887|2022-09-04 23:03:04
+//17:51:37,624 INFO  WordCountOutOfOrderSource [] - id: 7, word: a, frequency: 3, eventTime: 1662303776894|2022-09-04 23:02:56
+//17:51:38,630 INFO  WordCountOutOfOrderSource [] - id: 8, word: a, frequency: 1, eventTime: 1662303786891|2022-09-04 23:03:06
+//WordCountTimestamp{id='1,2,3,4,5,7', word='a', frequency=12, timestamp=1662303772840}
+//17:51:39,635 INFO  WordCountOutOfOrderSource [] - id: 9, word: a, frequency: 5, eventTime: 1662303778877|2022-09-04 23:02:58
+//17:51:40,641 INFO  WordCountOutOfOrderSource [] - id: 10, word: a, frequency: 4, eventTime: 1662303791904|2022-09-04 23:03:11
+//17:51:41,647 INFO  WordCountOutOfOrderSource [] - id: 11, word: a, frequency: 1, eventTime: 1662303795918|2022-09-04 23:03:15
+//17:51:42,653 INFO  WordCountOutOfOrderSource [] - id: 12, word: a, frequency: 6, eventTime: 1662303779883|2022-09-04 23:02:59
+//17:51:43,658 INFO  WordCountOutOfOrderSource [] - id: 13, word: a, frequency: 2, eventTime: 1662303846254|2022-09-04 23:04:06
+//WordCountTimestamp{id='6,8,10,11', word='a', frequency=8, timestamp=1662303784887}
+//WordCountTimestamp{id='13', word='a', frequency=2, timestamp=1662303846254}
